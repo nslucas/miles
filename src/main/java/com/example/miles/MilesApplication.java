e320e5b00311f067879e25fc80b24db0029bdf69
Lucas Nunes <lucasnunes.santos088@gmail.com>
2023-07-20 19:43:27 -0300

    project created

---
+++ src/main/java/com/example/miles/MilesApplication.java
@@ -0,0 +1,13 @@
+package com.example.miles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MilesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/miles/MilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
